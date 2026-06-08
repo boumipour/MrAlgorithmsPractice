f04cee7300fc8ac03e3947dcb4e2c0c617b79aee
Majid Boumipour <Majid.boumipour@freedos.it>
2026-06-08 12:19:36 +0200

    add 121 max profit

--- ArraySearch.cs
+++ ArraySearch.cs
@@ -26,4 +26,31 @@ public class ArraySearch
 
         return candidate;
     }
+
+    /// <summary>
+    /// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    /// Example 1:
+    /// Input: prices = [7,1,5,3,6,4]
+    /// Output: 5
+    /// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    /// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+    /// </summary>
+    /// <param name="prices"> You are given an array prices where prices[i] is the price of a given stock on the ith day.</param>
+    /// <returns>Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.</returns>
+     public int MaxProfit_121(int[] prices) {
+        
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (int price in prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+      return maxProfit;
+
+    }
 }
