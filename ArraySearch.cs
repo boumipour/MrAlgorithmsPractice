eeceae8dc4d30695d4e78583e23301bbe56cc206
Majid Boumipour <Majid.boumipour@freedos.it>
2026-06-09 14:22:54 +0200

    add 122 max profit 2

--- ArraySearch.cs
+++ ArraySearch.cs
@@ -37,20 +37,51 @@ public class ArraySearch
     /// </summary>
     /// <param name="prices"> You are given an array prices where prices[i] is the price of a given stock on the ith day.</param>
     /// <returns>Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.</returns>
-     public int MaxProfit_121(int[] prices) {
-        
+    public int MaxProfit_121(int[] prices)
+    {
+
         int minPrice = int.MaxValue;
         int maxProfit = 0;
 
-        foreach (int price in prices) {
-            if (price < minPrice) {
+        foreach (int price in prices)
+        {
+            if (price < minPrice)
+            {
                 minPrice = price;
-            } else if (price - minPrice > maxProfit) {
+            }
+            else if (price - minPrice > maxProfit)
+            {
                 maxProfit = price - minPrice;
             }
         }
 
-      return maxProfit;
+        return maxProfit;
 
+    }
+
+    /// <summary>
+    /// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+    /// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+    /// Find and return the maximum profit you can achieve.
+    ///Example 1:
+    ///Input: prices = [7,1,5,3,6,4]
+    ///Output: 7
+    ///Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+    ///Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+    ///Total profit is 4 + 3 = 7.
+    /// </summary>
+    /// <param name="prices">You are given an integer array prices where prices[i] is the price of a given stock on the ith day</param>
+    /// <returns>The maximum profit you can achieve</returns>
+    public int MaxProfit2_122(int[] prices)
+    {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+
+
+        return profit;
     }
 }
